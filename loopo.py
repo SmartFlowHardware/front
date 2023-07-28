eb54e0e1f88647547c102c3a8cac08ffd9074142
EnriquePasillas <71039023+EnriquePasillas@users.noreply.github.com>
2023-07-28 17:21:45 -0600

    Add files via upload

--- loopo.py
+++ loopo.py
@@ -1,12 +1,14 @@
 import serial
 
 # Configuración del puerto serial
-# port = '/dev/ttymxc1'  # Reemplaza con el puerto serial correspondiente en tu sistema
-# baudrate = 115200
-port = 'COM22'  # Reemplaza con el puerto serial correspondiente en tu sistema
+port = '/dev/ttymxc1'  # Reemplaza con el puerto serial correspondiente en tu sistema
 baudrate = 115200
+#port = 'COM15'  # Reemplaza con el puerto serial correspondiente en tu sistema
+#baudrate = 115200
 # Abre la conexión serial
 ser = serial.Serial(port, baudrate)
+start_scan='set scanner on 0\n'
+ser.write(start_scan.encode())
 
 # Función para leer la entrada del usuario y enviarla al puerto serial
 def send_input():
@@ -14,16 +16,19 @@ def send_input():
         while True:
             # Lee la entrada del usuario
             user_input = input('> ')
+            user_input=user_input+'\n'
 
             # Verifica si el usuario desea salir del terminal
-            if user_input.lower() == 'exit':
+            if user_input.lower() == 'exit\n':
                 break
 
             # Envía los datos al puerto serial
             ser.write(user_input.encode())
 
     except KeyboardInterrupt:
+        
         pass
+        
 
     # Cierra la conexión serial
     ser.close()
